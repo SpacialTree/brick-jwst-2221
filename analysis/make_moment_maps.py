95ef88d95dfeee1f9115ed036a2d8fa4c92ca22c
Savannah Gramze <savannahgramze@c31a-s22.ufhpc>
2023-09-13 16:46:05 -0400

    edited make_moment_maps.py to include saving subcubes

--- analysis/make_moment_maps.py
+++ analysis/make_moment_maps.py
@@ -19,6 +19,7 @@ line = "HNCO" ## CHANGE THIS FOR OTHER LINES
 files = glob.glob('/orange/adamginsburg/jwst/cloudc/alma/cutouts/cloudc_*')
 
 # Read the region files
+print('Reading region files.')
 reg = regions.Regions.read('/orange/adamginsburg/jwst/cloudc/regions/nircam_cloudc_fov_perp.reg')
 reg_cloud = regions.Regions.read('/orange/adamginsburg/jwst/cloudc/regions/large_cloud.reg')
 reg_sub = regions.Regions.read('/orange/adamginsburg/jwst/cloudc/regions/sub_cloud.reg')
@@ -29,6 +30,7 @@ reg_double = regions.Regions.read('/orange/adamginsburg/jwst/cloudc/regions/doub
 reg_fov = regions.Regions.read('/orange/adamginsburg/jwst/cloudc/regions/fov.reg')
 
 ### Cubes
+print('Opening Cubes')
 cube_full = SpectralCube.read(filename_full).with_spectral_unit(u.km/u.s, velocity_convention='radio')
 
 cube_full_reg = cube_full.subcube_from_regions(reg, minimize=False)
@@ -39,20 +41,34 @@ cube_full_filament = cube_full.subcube_from_regions(reg_filament, minimize=False
 cube_full_double = cube_full.subcube_from_regions(reg_double, minimize=False).spectral_slab(0*u.km/u.s, 45*u.km/u.s)
 
 ### Moment Maps - put these in order of least and most likely to crash
+print('Making moment map for double peaked cloud.')
 mom0_full_double = cube_full_double.moment0()
-mom0_full_double.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_downsampled_{line}_double.fits')
+mom0_full_double.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_{line}_double.fits')
 
+print('Making moment map for smaller sub cloud.')
 mom0_full_sub = cube_full_sub.moment0()
-mom0_full_sub.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_downsampled_{line}_sub.fits')
+mom0_full_sub.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_{line}_sub.fits')
 
+print('Making moment map for filament.')
 mom0_full_filament = cube_full_filament.moment0()
-mom0_full_filament.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_downsampled_{line}_filament.fits')
+mom0_full_filament.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_{line}_filament.fits')
 
+print('Making moment map for high mass star formation region.')
 mom0_full_hmsfr = cube_full_hmsfr.moment0()
-mom0_full_hmsfr.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_downsampled_{line}_hmsfr.fits')
+mom0_full_hmsfr.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_{line}_hmsfr.fits')
 
+print('Making moment map for larger cloud.')
 mom0_full_cloud = cube_full_cloud.moment0()
-mom0_full_cloud.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_downsampled_{line}_cloud.fits')
+mom0_full_cloud.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_{line}_cloud.fits')
 
+print('Making moment map for full fov of jwst data.')
 mom0_full_reg = cube_full_reg.moment0()
-mom0_full_reg.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_downsampled_{line}_reg.fits')
+mom0_full_reg.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/mom0_{line}_reg.fits')
+
+### Attempting to write the cubes
+cube_full_double.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/cube_{line}_double.fits')
+cube_full_sub.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/cube_{line}_sub.fits')
+cube_full_filament.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/cube_{line}_filament.fits')
+cube_full_hmsfr.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/cube_{line}_hmsfr.fits')
+cube_full_cloud.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/cube_{line}_cloud.fits')
+cube_full_reg.write(f'/orange/adamginsburg/jwst/cloudc/alma/moments/cube_{line}_reg.fits')
