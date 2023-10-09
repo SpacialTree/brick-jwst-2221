9e0f7821a85ed2da7367685b1a599141f01ae651
Savannah Gramze <savannahgramze@c0713a-s27.ufhpc>
2023-10-09 08:51:06 -0400

    adding some files

---
+++ analysis/make_moments_hmsfr.py
@@ -0,0 +1,16 @@
+from spectral_cube import SpectralCube
+import numpy as np
+from astropy.visualization import quantity_support
+from astropy import units as u
+from astropy import wcs
+import matplotlib.pyplot as plt
+from astropy.utils import data
+from reproject import reproject_exact
+from astropy.io import fits
+import regions
+import glob, os
+import spectral_cube
+
+reg_hmsfr = regions.Regions.read('/orange/adamginsburg/jwst/cloudc/regions/hmsfr.reg')
+
+
