6ef1d9ea54df28d427fa3864dbd03a80f311a1bd
Savannah Gramze <savannahgramze@c0704a-s10.ufhpc>
2024-07-22 14:56:04 -0400

    7.22.2024

--- analysis/extract_spectra.py
+++ analysis/extract_spectra.py
@@ -21,29 +21,38 @@ def extract_spec(cube, pos, rad):
     average_spectrum = subcube.mean(axis=(1, 2))
     return average_spectrum
 
-pos_cloudc1 = SkyCoord('17:46:21.3048683891', '-28:35:33.1211282499', unit=(u.hourangle, u.deg))
-pos_cloudc2 = SkyCoord('17:46:18.3316118680', '-28:34:48.4717811920', unit=(u.hourangle, u.deg))
-pos_cloudd = SkyCoord('17:46:22.6563371259', '-28:33:27.5405071803', unit=(u.hourangle, u.deg))
-pos_filament = SkyCoord('17:46:20.9063719501', '-28:37:51.6942550990', unit=(u.hourangle, u.deg))
+def extract_spectrum(obj, fn, line_name='HNCO', restfreq=87.925238*u.GHz):
+    cube = SpectralCube.read(fn)
+    cube = cube.with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=restfreq)
+    pos = SkyCoord(obj['ra'], obj['dec'], unit=(u.hourangle, u.deg))
+    rad = obj['rad']*u.arcsec
+    average_spectrum = extract_spec(cube, pos, rad)
+    average_spectrum.write(f'/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_{obj["name"]}_{line_name}.fits')
+    #return average_spectrum
 
-rad_cloudc1 = 30*u.arcsec
-rad_cloudc2 = 30*u.arcsec
-rad_cloudd = 60*u.arcsec
-rad_filament = 1.5*u.arcmin
+cloudc1 = {'name': 'cloudc1', 'ra': '17:46:21.3048683891', 'dec': '-28:35:33.1211282499', 'rad': 30}
+cloudc2 = {'name': 'cloudc2', 'ra': '17:46:18.3316118680', 'dec': '-28:34:48.4717811920', 'rad': 30}
+cloudd = {'name': 'cloudd', 'ra': '17:46:22.6563371259', 'dec': '-28:33:27.5405071803', 'rad': 60}
+filament = {'name': 'filament', 'ra': '17:46:20.9063719501', 'dec': '-28:37:51.6942550990', 'rad': 90}
 
-cube = SpectralCube.read('/orange/adamginsburg/ACES/mosaics/cubes/HNCO_7m12mTP_CubeMosaic.fits')
+fn_HNCO = '/orange/adamginsburg/ACES/mosaics/cubes/HNCO_7m12mTP_CubeMosaic.fits'
+fn_12CO = '/orange/adamginsburg/cmz/nobeyama/12CO-2.BEARS.FITS'
 #cube = cube.with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=87.925238*u.GHz)
 
 # Cloud c1
-average_spectrum_cloudc1 = extract_spec(cube, pos_cloudc1, rad_cloudc1)
-average_spectrum_cloudc1.write('/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_cloudc1_HNCO.fits')
+extract_spectrum(cloudc1, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
+extract_spectrum(cloudc1, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
 
-average_spectrum_cloudc2 = extract_spec(cube, pos_cloudc2, rad_cloudc2)
-average_spectrum_cloudc2.write('/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_cloudc2_HNCO.fits')
+# Cloud c2
+extract_spectrum(cloudc2, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
+extract_spectrum(cloudc2, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
 
-average_spectrum_cloudd = extract_spec(cube, pos_cloudd, rad_cloudd)
-average_spectrum_cloudd.write('/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_cloudd_HNCO.fits')
+# Cloud d
+extract_spectrum(cloudd, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
+extract_spectrum(cloudd, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
 
-average_spectrum_filament = extract_spec(cube, pos_filament, rad_filament)
-average_spectrum_filament.write('/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_filament_HNCO.fits')
+# Filament
+extract_spectrum(filament, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
+extract_spectrum(filament, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
+
 
