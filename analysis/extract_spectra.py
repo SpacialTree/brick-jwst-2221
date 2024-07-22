60657719e0a8f5a59158cf9ab90a6017c4d59f15
Savannah Gramze <savannahgramze@c0704a-s10.ufhpc>
2024-07-22 16:40:03 -0400

    made function and dictionary

--- analysis/extract_spectra.py
+++ analysis/extract_spectra.py
@@ -27,7 +27,7 @@ def extract_spectrum(obj, fn, line_name='HNCO', restfreq=87.925238*u.GHz):
     pos = SkyCoord(obj['ra'], obj['dec'], unit=(u.hourangle, u.deg))
     rad = obj['rad']*u.arcsec
     average_spectrum = extract_spec(cube, pos, rad)
-    average_spectrum.write(f'/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_{obj["name"]}_{line_name}.fits')
+    average_spectrum.write(f'/orange/adamginsburg/jwst/cloudc/alma/spectra/spectrum_{obj["name"]}_{line_name}.fits', overwrite=True)
     #return average_spectrum
 
 cloudc1 = {'name': 'cloudc1', 'ra': '17:46:21.3048683891', 'dec': '-28:35:33.1211282499', 'rad': 30}
@@ -39,20 +39,26 @@ fn_HNCO = '/orange/adamginsburg/ACES/mosaics/cubes/HNCO_7m12mTP_CubeMosaic.fits'
 fn_12CO = '/orange/adamginsburg/cmz/nobeyama/12CO-2.BEARS.FITS'
 #cube = cube.with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=87.925238*u.GHz)
 
+## CO
+# Cloud c1
+extract_spectrum(cloudc1, fn_12CO, line_name='12CO-BEARS', restfreq=1.152712040000E+11*u.Hz)
+# Cloud c2
+extract_spectrum(cloudc2, fn_12CO, line_name='12CO-BEARS', restfreq=1.152712040000E+11*u.Hz)
+# Cloud d
+extract_spectrum(cloudd, fn_12CO, line_name='12CO-BEARS', restfreq=1.152712040000E+11*u.Hz)
+# Filament
+extract_spectrum(filament, fn_12CO, line_name='12CO-BEARS', restfreq=1.152712040000E+11*u.Hz)
+
+## HNCO
 # Cloud c1
 extract_spectrum(cloudc1, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
-extract_spectrum(cloudc1, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
-
 # Cloud c2
 extract_spectrum(cloudc2, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
-extract_spectrum(cloudc2, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
-
 # Cloud d
 extract_spectrum(cloudd, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
-extract_spectrum(cloudd, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
-
 # Filament
 extract_spectrum(filament, fn_HNCO, line_name='HNCO', restfreq=87.925238*u.GHz)
-extract_spectrum(filament, fn_12CO, line_name='12CO', restfreq=1.152712040000E+11*u.Hz)
+
+
 
 
